51c4e95e55190e2a4634f6fef6fede61d6cefee9
ZenithDevHQ <ZenithDevHQ@users.noreply.github.com>
2026-02-28 20:58:49 -0500

    test: add JUnit 5 + Mockito infrastructure and utility tests
    - Add JUnit 5, JUnit params, Mockito to build.gradle
    - Add TimeUtilTest with 12 tests (format + parse edge cases)
    - Add ColorUtilTest with parameterized tests for all 16 color codes

---
+++ src/test/java/com/zib/playtime/util/TimeUtilTest.java
@@ -0,0 +1,67 @@
+package com.zib.playtime.util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeUtilTest {
+
+    @Test
+    void formatZero() {
+        assertEquals("0h 0m", TimeUtil.format(0));
+    }
+
+    @Test
+    void formatMinutesOnly() {
+        assertEquals("0h 30m", TimeUtil.format(30 * 60 * 1000L));
+    }
+
+    @Test
+    void formatHoursAndMinutes() {
+        assertEquals("2h 15m", TimeUtil.format((2 * 60 + 15) * 60 * 1000L));
+    }
+
+    @Test
+    void formatLargeValue() {
+        assertEquals("100h 0m", TimeUtil.format(100 * 60 * 60 * 1000L));
+    }
+
+    @Test
+    void parseSeconds() {
+        assertEquals(5000, TimeUtil.parseTime("5s"));
+    }
+
+    @Test
+    void parseMinutes() {
+        assertEquals(30 * 60 * 1000L, TimeUtil.parseTime("30m"));
+    }
+
+    @Test
+    void parseHours() {
+        assertEquals(2 * 60 * 60 * 1000L, TimeUtil.parseTime("2h"));
+    }
+
+    @Test
+    void parseDays() {
+        assertEquals(24 * 60 * 60 * 1000L, TimeUtil.parseTime("1d"));
+    }
+
+    @Test
+    void parseNull() {
+        assertEquals(-1, TimeUtil.parseTime(null));
+    }
+
+    @Test
+    void parseEmpty() {
+        assertEquals(-1, TimeUtil.parseTime(""));
+    }
+
+    @Test
+    void parseInvalid() {
+        assertEquals(-1, TimeUtil.parseTime("abc"));
+    }
+
+    @Test
+    void parseNoUnit() {
+        assertEquals(500, TimeUtil.parseTime("500"));
+    }
+}
